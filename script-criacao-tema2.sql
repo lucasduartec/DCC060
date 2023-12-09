1b3947451765f414d374dcc05cecf7dc00023637
Lucas Duarte <104040764+lucasduartec@users.noreply.github.com>
2023-12-09 19:39:35 -0300

    Add files via upload

---
+++ script-criacao-tema2.sql
@@ -0,0 +1,35 @@
+CREATE TABLE cliente (
+		id_cliente serial not null primary key,
+		nome text not null, 
+		sobrenome text not null, 
+		telefone text not null, 
+		email text not null
+);
+
+CREATE TABLE pedido (
+		id_pedido serial not null primary key,
+		id_cliente integer not null,		/* ADICIONAR CONSTRAINT DE FOREIGN KEY */
+		data_pedido date not null,
+		estado_pedido integer not null
+);
+
+CREATE TABLE item_menu (
+		id_item serial not null primary key,
+		nome_item text not null,
+		preco_item numeric (10,2) not null,
+		categoria_item text not null
+);
+
+CREATE TABLE funcionario(
+		id_funcionario serial not null primary key,
+		nome text not null,
+		sobrenome text not null,
+		cargo text not null,
+		salario numeric (10,2) not null
+);
+
+CREATE TABLE mesa (
+		id_mesa serial not null primary key,
+		numero_mesa integer not null,
+		capacidade integer not null
+);
